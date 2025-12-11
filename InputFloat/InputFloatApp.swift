9182f7876ea9d616b1ee71a1ad9162146c3eadb0
hi-dka <hidka@hidka.com>
2025-12-11 18:36:03 +0800

    Initial Commit

---
+++ InputFloat/InputFloatApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputFloatApp.swift
+//  InputFloat
+//
+//  Created by 杨鑫鑫 on 2025/12/11.
+//
+
+import SwiftUI
+
+@main
+struct InputFloatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
